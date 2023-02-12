292737b4db075f3dcb5f6a563b34345437ece2a8
Ivan Pozdin <ivanllpozdin@gmail.com>
2023-02-12 15:51:52 +0300

    feat: 2d and 3rd parts of task

--- Program.cs
+++ Program.cs
@@ -3,6 +3,4 @@
 using WebPracticeVK;
 
 var app = new VKWebApp();
-//app.StartConsoleApp();
-app.PrintFriendsOnline();
-//app.FetchUserInfo("bce93f7cbce93f7cbce93f7c21bffb42c5bbce9bce93f7cdf09201372391e47c1d5f5ad", "id250235208");
+app.StartConsoleApp();
--- VKWebApp.cs
+++ VKWebApp.cs
@@ -5,59 +5,131 @@ namespace WebPracticeVK;
 
 public class VKWebApp
 {
-    private readonly HttpClient client = new();
+    private readonly HttpClient _client = new();
 
-    public async void FetchUserInfo(string ACCESS_TOKEN, string user)
+    private async void FetchUserInfo(string accessToken, string user)
     {
-        var responseMessage = client
+        var responseMessage = _client
             .GetAsync(
-                $"https://api.vk.com/method/users.get?user_ids={user}&fields=bdate&access_token={ACCESS_TOKEN}&v=5.131")
+                $"https://api.vk.com/method/users.get?user_ids={user}&fields=bdate&access_token={accessToken}&v=5.131")
             .Result;
         var text = await responseMessage.Content.ReadAsStringAsync();
-        dynamic jsonDe = JsonConvert.DeserializeObject(text);
-        // string bday = jsonDe.response[0].bdate;
-        string firstName = jsonDe.response[0].first_name;
-        string lastName = jsonDe.response[0].last_name;
-
-        // Console.WriteLine(text);
-        Console.WriteLine($"Имя и Фамилия: {firstName} {lastName}");
+        dynamic jsonDe = JsonConvert.DeserializeObject(text) ?? throw new InvalidOperationException();
+        string birthday = jsonDe.response[0].bdate ?? "no data";
+        string firstName = jsonDe.response[0].first_name ?? "no data";
+        string lastName = jsonDe.response[0].last_name ?? "no data";
+        Console.WriteLine($"имя и фамилия, дата рождения: {firstName} {lastName}, {birthday}");
     }
 
     public void StartConsoleApp()
     {
-        Console.WriteLine("Введите сервисный ключ:");
-        var ACCESS_TOKEN = Console.ReadLine();
-        // string ACCESS_TOKEN = "bce93f7cbce93f7cbce93f7c21bffb42c5bbce9bce93f7cdf09201372391e47c1d5f5ad";
-        Console.WriteLine("Введите id пользователя:");
-        var user = Console.ReadLine();
-        // string user = "ivanpozdin";
+        Console.WriteLine(
+            "Всего есть 3 сценария работы:\n1. Получение открытой информации о пользователе.\n2. Список контактов онлайн.\n3. Поменять аватарку.");
+        var workScenario = " ";
+        while (workScenario != "1" && workScenario != "2" && workScenario != "3")
+        {
+            Console.WriteLine("Введите сценарий работы одной цифрой: ");
+            workScenario = Console.ReadLine();
+        }
 
-        FetchUserInfo(ACCESS_TOKEN, user);
+        switch (workScenario)
+        {
+            case "1":
+                DoScenario1();
+                break;
+            case "2":
+                DoScenario2();
+                break;
+            case "3":
+                DoScenario3();
+                break;
+        }
     }
 
-    public async void PrintFriendsOnline()
+    private void DoScenario1()
     {
-        /*var authString =
-            "https://oauth.vk.com/authorize?client_id=51543481&display=page&redirect_uri=https://oauth.vk.com/blank.html/&scope=friends&response_type=token&v=5.131&state=123456&revoke=1";
+        Console.WriteLine("Введите сервисный ключ:");
+        var accessToken = Console.ReadLine() ?? throw new ArgumentNullException();
+        Console.WriteLine("Введите id пользователя:");
+        var user = Console.ReadLine();
+        if (user != null) FetchUserInfo(accessToken, user);
+    }
+
+    private void DoScenario2()
+    {
+        var accessToken = GetAccessToken(true);
+        Console.WriteLine("Введите сервисный ключ");
+        var serviceKey = Console.ReadLine();
+        PrintFriendsOnline(accessToken ?? throw new InvalidOperationException(),
+            serviceKey ?? throw new InvalidOperationException());
+    }
+
+    private void DoScenario3()
+    {
+        var accessToken = GetAccessToken(photos: true);
+        var serverAddress = GetServerAddress(accessToken ?? throw new InvalidOperationException()).Result;
+        Console.WriteLine("Введите путь к файлу фотографии");
+        string photoPath = Console.ReadLine() ?? throw new InvalidOperationException();
+        Console.WriteLine("Введите название файла фотографии");
+        string photoName = Console.ReadLine() ?? throw new InvalidOperationException();
+        ChangeAvatar(accessToken ?? throw new InvalidOperationException(), serverAddress, photoPath, photoName);
+    }
+
+    private async void PrintFriendsOnline(string accessToken, string serviceKey)
+    {
+        var responseMessage = _client
+            .GetAsync($"https://api.vk.com/method/friends.getOnline?access_token={accessToken}&v=5.131").Result;
+        var text = await responseMessage.Content.ReadAsStringAsync();
+        Console.WriteLine(text);
+        dynamic jsonDe = JsonConvert.DeserializeObject(text) ?? throw new InvalidOperationException();
+        foreach (var id in jsonDe.response) FetchUserInfo(serviceKey, "id" + id);
+    }
+
+    private string? GetAccessToken(bool friends = false, bool photos = false)
+    {
+        var scope = "";
+        if (photos) scope = "photos";
+        if (friends) scope = "friends";
+        var authString =
+            $"https://oauth.vk.com/authorize?client_id=51543481&display=page&redirect_uri=https://oauth.vk.com/blank.html/&scope={scope}s&response_type=token&v=5.131&state=123456&revoke=1";
         authString = authString.Replace("&", "^&");
         Process.Start(new ProcessStartInfo(
                 "cmd",
                 $"/c start {authString}")
             { CreateNoWindow = true });
-        Console.WriteLine("Скопируйте и вставьте access token из адресной строки браузера после слова code= "); */
-        // var accessToken = Console.ReadLine();
-        // Console.WriteLine("Введите сервисный ключ");
-        // var serviceKey = Console.ReadLine();
-        var accessToken = "vk1.a.nzIlpBnGv7UPticuN6LlyImq7A_JWgUEr8Ak7t5ThbEj-bxKB826a9qBHe0wJB534TMVjf2Ds2bHQY7G4qEqQz_B86TCZpSE4IU2qLDaKyFW_i1b2a4IMk3tCBFOJoF_xzDpmEsDJupWltGup9i9-91bh8icfzmTHFLQPkcws-GWxhgXnhhoVoUW5FCeKDpy";
-        var serviceKey = "bce93f7cbce93f7cbce93f7c21bffb42c5bbce9bce93f7cdf09201372391e47c1d5f5ad";
-        var responseMessage = client
-            .GetAsync($"https://api.vk.com/method/friends.getOnline?access_token={accessToken}&v=5.131").Result;
-        var text = await responseMessage.Content.ReadAsStringAsync();
-        Console.WriteLine(text);
-        dynamic jsonDe = JsonConvert.DeserializeObject(text);
-        foreach (var id in jsonDe.response) FetchUserInfo(serviceKey, "id"+id);
-        
+        Console.WriteLine(
+            "Скопируйте и вставьте access token из адресной строки браузера после access_token= и до &expires_in=");
+        var accessToken = Console.ReadLine();
+        return accessToken;
+    }
+
+    private async Task<string> GetServerAddress(string accessToken)
+    {
+        var serverAddressResponse = _client
+            .GetAsync($"https://api.vk.com/method/photos.getOwnerPhotoUploadServer?access_token={accessToken}&v=5.131")
+            .Result;
+        var responseMessage = await serverAddressResponse.Content.ReadAsStringAsync();
+        dynamic serverAddressJsonDe = JsonConvert.DeserializeObject(responseMessage) ?? throw new InvalidOperationException();
+        string serverAddress = serverAddressJsonDe.response.upload_url;
+        return serverAddress;
+    }
+    private async void ChangeAvatar(string accessToken, string serverAddress,string photoPath, string photoName)
+    {
+        var requestContent = new MultipartFormDataContent
+        {
+            { new ByteArrayContent(await File.ReadAllBytesAsync(photoPath)), "photo", photoName }
+        };
+        var uploadResponse = _client.PostAsync(serverAddress, requestContent).Result;
+        var uploadResponseMessage = await uploadResponse.Content.ReadAsStringAsync();
+        dynamic uploadJsonDe = JsonConvert.DeserializeObject(uploadResponseMessage) ?? throw new InvalidOperationException();
+        Debug.Assert(uploadJsonDe != null, nameof(uploadJsonDe) + " != null");
+        string server = uploadJsonDe.server;
+        string photo = uploadJsonDe.photo;
+        string hash = uploadJsonDe.hash;
+        var saveResponse = _client
+            .GetAsync(
+                $"https://api.vk.com/method/photos.saveOwnerPhoto?access_token={accessToken}&server={server}&hash={hash}&photo={photo}&v=5.131")
+            .Result;
+        // var saveResponseMessage = await saveResponse.Content.ReadAsStringAsync();
     }
 }
-// vk1.a.nzIlpBnGv7UPticuN6LlyImq7A_JWgUEr8Ak7t5ThbEj-bxKB826a9qBHe0wJB534TMVjf2Ds2bHQY7G4qEqQz_B86TCZpSE4IU2qLDaKyFW_i1b2a4IMk3tCBFOJoF_xzDpmEsDJupWltGup9i9-91bh8icfzmTHFLQPkcws-GWxhgXnhhoVoUW5FCeKDpy
-// bce93f7cbce93f7cbce93f7c21bffb42c5bbce9bce93f7cdf09201372391e47c1d5f5ad
